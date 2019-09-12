2a9f1bad6ba504f41f05077c5fa64c99275509e6
shreya suman <shreyasumanck@gmail.com>
2019-09-12 10:25:18 -0400

    Initial commit

---
+++ assets/javascript/home.js
@@ -0,0 +1,16 @@
+// slideshow- change image every 3 seconds
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementById("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 3000);
+}
